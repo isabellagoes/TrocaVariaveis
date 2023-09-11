578312bdf1b1625dab1cdfedd37e2b0358dd96d6
Isabella Goes <isabellagoes.igs@gmail.com>
2023-09-10 22:08:45 -0300

    Troca de nomese

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿﻿string nome1, nome2, apoio;
+
+Console.WriteLine("Troca de Valores");
+
+Console.Write("Digite um nome: ");
+nome1 = Console.ReadLine()!;
+Console.Write("Digite outro nome: ");
+nome2 = Console.ReadLine()!;
+
+apoio = nome1;
+nome1 = nome2;
+nome2 = apoio;
+
+Console.WriteLine("\nTrocando os valores, temos:\n");
+
+Console.WriteLine($"NOME 1 = {nome1}");
+Console.WriteLine($"NOME 2 = {nome2}");
